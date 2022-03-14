7af174882324c208f17a436ae879aa7e030e30d3
layonfonseca <layon.fonseca@eldorado.org.br>
2022-03-14 17:37:35 -0300

    Curso de Flutter #31 - [Conversor de Moedas] Alinhando os itens da View

--- lib/app/models/views/home_view.dart
+++ lib/app/models/views/home_view.dart
@@ -7,26 +7,52 @@ class HomeView extends StatelessWidget {
         body: SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          Image.asset('assets/logo.png'),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: [
-                Expanded(
+      child: Padding(
+        padding:
+            const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
+        child: Column(
+          children: [
+            Image.asset('assets/logo.png', width: 250, height: 250),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                      child: SizedBox(
+                    height: 56,
                     child: DropdownButton<String>(
-                        items: [], onChanged: (value) {})),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(),
-                  ),
-                )
-              ],
+                        isExpanded: true,
+                        underline: Container(
+                          height: 1,
+                          color: Colors.amber,
+                        ),
+                        items: [
+                          DropdownMenuItem(child: Text('Real'), value: "Real"),
+                          DropdownMenuItem(
+                              child: Text('Dolar'), value: "Dolar"),
+                        ],
+                        onChanged: (value) {}),
+                  )),
+                  SizedBox(width: 10),
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber)),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber))),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          RaisedButton(onPressed: () {}, child: Text('CONVERTER'))
-        ],
+            SizedBox(height: 50),
+            RaisedButton(
+                color: Colors.amber, onPressed: () {}, child: Text('CONVERTER'))
+          ],
+        ),
       ),
     ));
   }
