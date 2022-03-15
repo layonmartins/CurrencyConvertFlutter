8a31b53c2bdeb8fef0ffe3fced51b11ae02c5c10
layonfonseca <layon.fonseca@eldorado.org.br>
2022-03-15 15:38:05 -0300

    Curso de Flutter #32 - [Conversor de Moedas] Componentizando a View

--- lib/app/models/views/home_view.dart
+++ lib/app/models/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:conversor_de_moedas/app/components/currency_box.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,46 +10,15 @@ class HomeView extends StatelessWidget {
       height: MediaQuery.of(context).size.height,
       child: Padding(
         padding:
-            const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
+            const EdgeInsets.only(left: 30, right: 30, top: 50, bottom: 20),
         child: Column(
           children: [
             Image.asset('assets/logo.png', width: 250, height: 250),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: SizedBox(
-                    height: 56,
-                    child: DropdownButton<String>(
-                        isExpanded: true,
-                        underline: Container(
-                          height: 1,
-                          color: Colors.amber,
-                        ),
-                        items: [
-                          DropdownMenuItem(child: Text('Real'), value: "Real"),
-                          DropdownMenuItem(
-                              child: Text('Dolar'), value: "Dolar"),
-                        ],
-                        onChanged: (value) {}),
-                  )),
-                  SizedBox(width: 10),
-                  Expanded(
-                    flex: 2,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.amber)),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.amber))),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 50),
+            SizedBox(height: 10),
+            CurrencyBox(),
+            SizedBox(height: 10),
+            CurrencyBox(),
+            SizedBox(height: 10),
             RaisedButton(
                 color: Colors.amber, onPressed: () {}, child: Text('CONVERTER'))
           ],
